04cd77bd28e5da1c90ce6b6709282579ecce36b0
Lodos <o.ueguer@gmail.com>
2021-05-21 01:17:56 +0200

    icon added

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Nav, Form, Navbar, NavDropdown, FormControl } from 'react-bootstrap';
 import "./searchBar.css";
 
 const SearchBar = (props) => {
@@ -10,32 +9,7 @@ const SearchBar = (props) => {
   return (
     <>
       <div>
-        <Form inline>
-          <FormControl
-            type="text"
-            placeholder="Search Restaurant"
-            className="mr-sm-2"
-            onChange={(e) => setRestaurantName(e.target.value)}
-          />
-          <Button
-            href={`/restaurant/${restaurantName}`}
-            variant="outline-success"
-          >
-            Search Restaurant
-          </Button>
-        </Form>
-        <Form inline>
-          <FormControl
-            type="text"
-            placeholder="Search City"
-            className="mr-sm-2"
-            onChange={(e) => setCityName(e.target.value)}
-          />
-          <Button href={`/home/${cityName}`} variant="outline-success">
-            Search City
-          </Button>
-        </Form>
-
+ 
         <button>Find</button>
         <input type="text" placeholder="find restaurants here..." />
 
